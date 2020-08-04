fa5d12c00ffc8c62f2684f01524f79a2ecd0755d
rochilandia <rochilandia@gmail.com>
2020-08-05 00:13:10 +0200

    Trabajo de grupo

---
+++ Proyecto/src/ejecutable.py
@@ -0,0 +1,18 @@
+
+import pandas as pd
+import numpy as np
+import requests
+import matplotlib.pyplot as plt
+import json
+
+
+url = ""
+def rosario (url):
+
+
+if __name__ == "__main__":
+
+
+
+data=pd.read_json('/Users/rochi/Desktop/TestFolder/DataScienceBootCampMIO/yotrabajoenCOVID19/Proyecto/src/c_averages.json', lines=True)
+print(data)
